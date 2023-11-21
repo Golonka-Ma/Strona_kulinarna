85a557cd5ef5752610e5a3936f2b2ded6722dc5d
Marcin Golonka <BattelShark@gmail.com>
2023-11-21 19:20:56 +0100

    homepage

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+function navigateTo(page) {
+    // Tu możesz dodać kod do przekierowywania do odpowiednich stron.
+    // Na razie jest tylko console.log na potrzeby przykładu.
+    console.log(`Navigating to ${page} page.`);
+}
+
+function searchRecipes() {
+    // Tu możesz dodać kod obsługujący wyszukiwanie przepisów.
+    // Na razie jest tylko console.log na potrzeby przykładu.
+    console.log('Searching for recipes.');
+}
